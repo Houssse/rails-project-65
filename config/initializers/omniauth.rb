f7c913811c6d7697c85d64aef161d518b3e635b0
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2025-01-05 09:00:54 +0300

    Fix: Resolved an authentication error

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@
 
 # config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: 'user' # rubocop:disable Style/FetchEnvVar
+  provider :github, ENV.fetch('GITHUB_CLIENT_ID', nil), ENV.fetch('GITHUB_CLIENT_SECRET', nil), scope: 'user'
 end
